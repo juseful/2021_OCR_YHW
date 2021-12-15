b90acbde79baa82634ea701e0d2d09b777710c33
yw0nam <spow2544@gmail.com>
2021-12-15 18:09:22 +0900

    update drawing bbx code

--- draw_bbx.py
+++ draw_bbx.py
@@ -1,34 +1,44 @@
 # %%
 import cv2
-import pandas as pd
 import json
 import numpy as np
 from glob import glob
 import matplotlib.pyplot as plt
+import os, argparse
+from tqdm import tqdm
 # %%
-cv2.__version__
-# %%
-annotation = glob('./medicine/annotations/*')
-# %%
-with open(annotation[0], "r") as json_file:
-    data = json.load(json_file)
-# %%
-image_root = './medicine/images/'
-image_name = data['images'][0]['name']
-img_path = image_root+image_name
-img = cv2.imread(img_path)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(img)
-# %%
-for dic in data['annotations'][0]['polygons']:
-    points = np.array(dic['points'], np.int32)
-    right_top = tuple(np.max(points, axis=0))
-    left_bottom = tuple(np.min(points, axis=0))
-    img = cv2.rectangle(img, tuple(left_bottom), tuple(right_top), 
-                         (0, 255, 0), 10)
-# %%
+def define_argparser():
+    p = argparse.ArgumentParser()
+    p.add_argument('--root_path', required=True)
+    p.add_argument('--save_path', required=True)
+    config = p.parse_args()
+    return config
 
-# %%
-plt.imshow(img)
-# %%
-plt.imsave('./temp.jpeg', img)
+def main(config):
+    if os.path.exists(config.save_path):
+        pass
+    else:
+        os.mkdir(config.save_path)
+    image_root = os.path.join(config.root_path, 'images')
+    annotation_path = glob(config.root_path+'/annotations/*')
+    # %%
+    for path in tqdm(annotation_path):
+        with open(path, "r") as json_file:
+            data = json.load(json_file)
+        for i in range(len(data['images'])):
+            # read image and store image name for reuse
+            image_name = data['images'][i]['name']
+            img = cv2.imread(os.path.join(image_root,image_name))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            # Draw bounding box and save
+            for dic in data['annotations'][i]['polygons']:
+                points = np.array(dic['points'], np.int32)
+                right_top = tuple(np.max(points, axis=0))
+                left_bottom = tuple(np.min(points, axis=0))
+                img = cv2.rectangle(img, tuple(left_bottom), tuple(right_top), 
+                                    (0, 255, 0), 10)
+            plt.imsave(os.path.join(config.save_path, image_name), img)
+
+if __name__ == "__main__":
+    config = define_argparser()
+    main(config)
