0ffcce48f7ea96eee90dc97503bf1703d9687337
KyungHyun Lee <lkh256@gmail.com>
2021-12-16 22:21:09 +0900

    make in fuction and split into easyocr and keras type

--- generate_result.py
+++ generate_result.py
@@ -3,29 +3,76 @@ import pandas as pd
 import json
 from tqdm import tqdm
 # %%
-DATAPATH = "./DATA/text_export_result_easyocr_word.xlsx"
-sheet_names = pd.ExcelFile(DATAPATH).sheet_names
-# %%
+def generate_json_keras(data, result_file_nm):
 
-result = {}
+    sheet_names = pd.ExcelFile(data).sheet_names
+    result = {}
 
-for sheet_nm in tqdm(sheet_names):
-    df_tmp = pd.read_excel(DATAPATH, sheet_name=sheet_nm)
-    
-    dict_tmp = {'image_name':df_tmp['filename'][0], 
-                'bbox':df_tmp['bbox'].to_list()}
-    
-    for key, value in dict_tmp.items():
-        if key in result:
-            if isinstance(result[key], list):
-                result[key].append(value)
+    for sheet_nm in tqdm(sheet_names):
+        
+        df_tmp = pd.read_excel(data, sheet_name=sheet_nm)
+        
+        dict_tmp = {'image_name':df_tmp['filename'][0], 
+                    'bbox':df_tmp['bbox'].to_list(),
+                    'x_start':df_tmp['x_start'].to_list(),
+                    'y_start':df_tmp['y_start'].to_list(),
+                    'x_end':df_tmp['x_end'].to_list(),
+                    'y_end':df_tmp['y_end'].to_list(),
+                    'text':df_tmp['text'].to_list()}
+        
+        
+        for key, value in dict_tmp.items():
+            if key in result:
+                if isinstance(result[key], list):
+                    result[key].append(value)
+                else:
+                    tmp_list = [result[key]]
+                    tmp_list.append(value)
+                    result[key] = tmp_list
             else:
-                tmp_list = [result[key]]
-                tmp_list.append(value)
-                result[key] = tmp_list
-        else:
-            result[key] = value
+                result[key] = value
+            
+    with open(result_file_nm, 'w', encoding='utf-8') as fp:
+        json.dump(result, fp, indent=4, ensure_ascii=False)
+        fp.close()
+
+
+def generate_json_easyocr(data, result_file_nm):
+
+    sheet_names = pd.ExcelFile(data).sheet_names
+    result = {}
+
+    for sheet_nm in tqdm(sheet_names):
+        
+        df_tmp = pd.read_excel(data, sheet_name=sheet_nm)
+        
+        dict_tmp = {'image_name':df_tmp['filename'][0], 
+                    'bbox':df_tmp['bbox'].to_list(),
+                    'x_start':df_tmp['x_start'].to_list(),
+                    'y_start':df_tmp['y_start'].to_list(),
+                    'x_end':df_tmp['x_end'].to_list(),
+                    'y_end':df_tmp['y_end'].to_list(),
+                    'easyocr_text':df_tmp['easyocr_text'].to_list(), 
+                    'tesseract_text':df_tmp['tesseract_text'].to_list(),}
+        
+        
+        for key, value in dict_tmp.items():
+            if key in result:
+                if isinstance(result[key], list):
+                    result[key].append(value)
+                else:
+                    tmp_list = [result[key]]
+                    tmp_list.append(value)
+                    result[key] = tmp_list
+            else:
+                result[key] = value
+            
+    with open(result_file_nm, 'w', encoding='utf-8') as fp:
+        json.dump(result, fp, indent=4, ensure_ascii=False)
+        fp.close()
+
 # %%
-with open('result.json', 'w') as fp:
-    json.dump(result, fp, indent=4)
-# %%
+if __name__ == '__main__':
+    generate_json_keras("./DATA/text_export_result_kerasocr.xlsx", './JSON/result_kerasocr.json')
+    generate_json_easyocr("./DATA/text_export_result_easyocr_line.xlsx", './JSON/result_easyocr_line.json')
+    generate_json_easyocr("./DATA/text_export_result_easyocr_word.xlsx", './JSON/result_easyocr_word.json')
